1bddb444f78174d5007429b97c5139aac896243a
Maratmost <maratmost89@gmail.com>
2023-01-25 20:42:28 +0300

    Lesson 2.2.1. mistake

---
+++ Lesson 2.2.1./Lesson 2.2.1./ViewController.swift
@@ -0,0 +1,73 @@
+//
+//  ViewController.swift
+//  Lesson 2.2.1.
+//
+//  Created by maratmost on 27/01/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var colorView: UIView!
+    
+    @IBOutlet weak var redLabel: UILabel!
+    @IBOutlet weak var greenLabel: UILabel!
+    @IBOutlet weak var blueLabel: UILabel!
+    
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        colorView.layer.cornerRadius = 15
+        
+        redSlider.minimumTrackTintColor = .red
+        greenSlider.minimumTrackTintColor = .green
+        blueSlider.minimumTrackTintColor = .blue
+        
+        redSlider.value = 10
+        greenSlider.value = 60
+        blueSlider.value = 150
+        
+        setColor()
+        setValue(for: redLabel, greenLabel, blueLabel)
+        
+    }
+    
+    // Изменение цветов слайдерами
+    @IBAction func rgbSlider(_ sender: UISlider) {
+        setColor()
+        
+        switch sender.tag {
+        case 0: redLabel.text = string(from: sender)
+        case 1: greenLabel.text = string(from: sender)
+        case 2: blueLabel.text = string(from: sender)
+        default: break
+        }
+    }
+    
+    // Цвет вью
+    private func setColor() {
+        colorView.backgroundColor = UIColor(red: CGFloat(redSlider.value) / 255, green: CGFloat(greenSlider.value) / 255, blue: CGFloat(blueSlider.value) / 255, alpha: 1)
+    }
+    
+    private func setValue(for labels: UILabel...) {
+        labels.forEach { label in
+            switch label.tag {
+            case 0: redLabel.text = string(from: redSlider)
+            case 1: greenLabel.text = string(from: greenSlider)
+            case 2: blueLabel.text = string(from: blueSlider)
+            default: break
+            }
+        }
+    }
+    
+    // Значения RGB
+    private func string(from slider: UISlider) -> String {
+        String(Int(slider.value))
+    }
+    
+}
+
